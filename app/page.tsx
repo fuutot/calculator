3ca3e501cc450abcb62d7eed8b37b50e0787d8ac
fuutot <fuutt659@gmail.com>
2025-09-03 17:38:18 +0900

    add: 電卓のUIを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
 export default function Home() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div className="calculator-container">
+      <div className="calculator-display">{"0"}</div>
+      <div className="calculator-result">{"0"}</div>
+      <div className="calculator-buttons">
+        {[
+          "7",
+          "8",
+          "9",
+          "/",
+          "4",
+          "5",
+          "6",
+          "X",
+          "1",
+          "2",
+          "3",
+          "-",
+          "0",
+          ".",
+          "=",
+          "+",
+        ].map((btn) => (
+          <button
+            key={btn}
+            className={
+              btn === "=" ? "calculator-btn calculator-equal" : "calculator-btn"
+            }
+          >
+            {btn}
+          </button>
+        ))}
+        <button className="calculator-btn calculator-clear">Clear</button>
+      </div>
+    </div>
+  );
 }
