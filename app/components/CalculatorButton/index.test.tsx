8ed9d032f27bd85f53cc35d318c73edb70bcfba6
fuutot <fuutt659@gmail.com>
2025-09-13 10:54:06 +0900

    refactor: Reactのインポートを削除

--- app/components/CalculatorButton/index.test.tsx
+++ app/components/CalculatorButton/index.test.tsx
@@ -2,7 +2,6 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CalculatorButton, { CalculatorButtonProps } from "./index";
